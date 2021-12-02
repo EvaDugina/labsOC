85434cb1312dcbada2add7065450ccdc40954c27
JohnDugin <vania.dugin917@mail.ru>
2021-12-02 11:50:26 +0300

    lab3

---
+++ lab3/reader.c
@@ -0,0 +1,83 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <time.h>
+#include <errno.h>
+
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+
+#define MEM_NAME "writer.c"
+
+
+typedef struct data_struct {
+	time_t time;
+	pid_t pid;
+} d_struct;
+
+
+char* timeToChar(int num){
+	char* time = (char*) malloc(2);
+	if (num < 10){
+		time[0] = '0';
+		time[1] = num + '0';
+		return time;
+	}
+	sprintf(time, "%d", num);
+	return time;
+}
+
+int main(int argc, char** argv){
+
+	sleep(1);
+	
+	//while (1){
+
+		key_t key = ftok(MEM_NAME, 1);
+		if (key < 0){
+			printf("[READER] ftok() failed with code: %d\n", errno);
+			return -1;
+		}
+
+		int shmem_id = shmget(key, sizeof(d_struct), 0666);
+		if (shmem_id < 0) {
+			printf("ERROR!! shmget() failed with code: %d\n\n", errno);
+			return -1;
+		}
+
+		void* ptr_to_shm = shmat(shmem_id, NULL, 0);
+		if (ptr_to_shm == (void*)-1){
+			printf("ERROR!! shmat() failed with code: %d\n\n", errno);
+			return -1;
+		}
+			
+		sleep(2);
+		
+		d_struct g_data = *((d_struct*)ptr_to_shm);
+		
+		int p_hh = localtime(&g_data.time)->tm_hour;
+		int p_mm = localtime(&g_data.time)->tm_min;
+		int p_ss = localtime(&g_data.time)->tm_sec;
+			
+		time_t ch_time = time(NULL);
+		int hh = localtime(&ch_time)->tm_hour;
+		int mm = localtime(&ch_time)->tm_min;
+		int ss = localtime(&ch_time)->tm_sec;
+			
+		printf("----\nPARENT PID = %d\n", g_data.pid);
+		printf("PARENT TIME = %s::%s::%s\n", timeToChar(p_hh), timeToChar(p_mm), timeToChar(p_ss));
+		printf("CHILD TIME = %s::%s::%s\n\n", timeToChar(hh), timeToChar(mm), timeToChar(ss));
+
+				
+		shmdt(ptr_to_shm);
+		//shmctl(shmem_id, IPC_RMID, NULL);
+				
+	//}	
+
+	return 0;
+}
---
+++ lab3/writer.c
@@ -0,0 +1,73 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <time.h>
+#include <errno.h>
+
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+
+#define MEM_NAME "writer.c"
+
+
+typedef struct data_struct {
+	time_t time;
+	pid_t pid;
+} d_struct;
+
+int main(int argc, char** argv){
+
+	while (1){
+
+		struct shmid_ds shmid_struct;
+	
+		key_t key = ftok(MEM_NAME, 1);
+		if (key < 0){
+			printf("[WRITER] ftok() failed with code: %d\n", errno);
+			return -1;
+		}
+
+		int shmem_id = shmget(key, sizeof(d_struct), IPC_CREAT | 0666);		
+
+		if (shmem_id < 0) {
+			printf("ERROR!! shmget() failed with code: %d\n", errno);
+			return -1;
+		}
+
+		void* ptr_to_shm = shmat(shmem_id, NULL, 0);
+
+		shmctl(shmem_id, IPC_STAT, &shmid_struct);
+		
+		if (shmid_struct.shm_nattch > 1){
+			printf("ERROR!! [WORKER] is already exist\n");
+			return -1;
+		}
+		
+		if (ptr_to_shm == (void*)-1){
+			printf("ERROR!! shmat() failed with code: %d\n\n", errno);
+			return -1;
+		}
+			
+		time_t now_time = time(NULL);
+		d_struct data = {now_time, getpid()};
+				
+		*((d_struct*)ptr_to_shm) = data;
+		//memсpy(ptr_to_shm, data, sizeof(d_struct));
+
+		//sleep(4);
+		sleep(4);
+			
+		shmdt(ptr_to_shm);
+		shmctl(shmem_id, IPC_RMID, NULL);
+		
+		printf("----\n[WRITER]\nSended PID: %d\nSended TIME: %d\n\n", getpid(), now_time);
+			
+	}	
+
+	return 0;
+}
