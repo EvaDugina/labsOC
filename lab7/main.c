858c52e0435789d3e59549b1ac0db481f5fc8043
JohnDugin <vania.dugin917@mail.ru>
2021-12-16 20:21:26 +0300

    lab7

--- lab7/main.c
+++ lab7/main.c
@@ -5,24 +5,26 @@
 
 
 int counter = 0;
-pthread_mutex_t mutex;
+pthread_rwlock_t rwlock = PTHREAD_RWLOCK_INITIALIZER;
 
 void* _write(){
 	int i = 0;
 	while (i < 5) {
-		pthread_mutex_lock(&mutex);
+		pthread_rwlock_trywrlock(&rwlock);
 		counter += 1;
-		pthread_mutex_unlock(&mutex);
-		sleep(1);
+		pthread_rwlock_unlock(&rwlock);
 		i += 1;
+		sleep(1);
 	}
 }
 
 void* _read(){
-	while (1) {
-		pthread_mutex_lock(&mutex);
+	int i = 0;
+	while (i < 5) {
+		pthread_rwlock_tryrdlock(&rwlock);
 		printf("TID = %x\t\t||\tCOUNTER = %d\n", pthread_self(), counter);
-		pthread_mutex_unlock(&mutex);
+		pthread_rwlock_unlock(&rwlock);
+		i++;
 		sleep(1);
 	}
 	pthread_exit(NULL);
@@ -32,8 +34,6 @@ int main(){
 	pthread_t thread_w;
 	pthread_t array_thread_r[10];
 	
-	pthread_mutex_init(&mutex, NULL);
-
 	pthread_create(&thread_w, NULL, _write, NULL);
 	for(int i=0; i < 10; i++)
 		pthread_create(&array_thread_r[i], NULL, _read, NULL);
@@ -42,7 +42,7 @@ int main(){
 	for(int i=0; i < 10; i++)
 		pthread_join(array_thread_r[i], NULL);
 	
-	pthread_mutex_destroy(&mutex);
+	pthread_rwlock_destroy(&rwlock);
 
 	return 0;
 }
